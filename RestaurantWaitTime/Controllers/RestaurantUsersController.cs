cd26aa2e55f6cc0e5f365c6dc0a8e36a8f69418f
Wei <iqboss@gmail.com>
2016-05-05 15:01:19 -0400

    daily commit

--- RestaurantWaitTime/Controllers/RestaurantUsersController.cs
+++ RestaurantWaitTime/Controllers/RestaurantUsersController.cs
@@ -1,90 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server.Authentication;
 using RestaurantWaitTime.Models;
 
 namespace RestaurantWaitTime.Controllers
 {
     public class RestaurantUsersController : ApiController
     {
-        private RestaurantWaitTimeContext db = new RestaurantWaitTimeContext();
+        private readonly RestaurantWaitTimeContext _db = new RestaurantWaitTimeContext();
 
-        // GET: api/RestaurantUsers
-        public IQueryable<RestaurantUser> GetRestaurantUsers()
+        [HttpGet]
+        [Route("api/GetAllRestaurantUsers")]
+        public IQueryable GetAllRestaurantUsers()
         {
-            return db.RestaurantUsers;
+            return _db.RestaurantUsers
+                .Select(r => new
+                {
+                    r.UserId,
+                    r.IdpId,
+                    r.Name,
+                    r.Type,
+                    r.Email
+                });
         }
 
-        // GET: api/RestaurantUsers/5
+        [Route("api/GetRestaurantUser/{userId}")]
         [ResponseType(typeof(RestaurantUser))]
-        public async Task<IHttpActionResult> GetRestaurantUser(string id)
+        [HttpGet]
+        public async Task<IHttpActionResult> GetRestaurantUser(string userId)
         {
-            RestaurantUser restaurantUser = await db.RestaurantUsers.FindAsync(id);
+            User user = await _db.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [ResponseType(typeof(void))]
+        [HttpPatch]
+        public async Task<IHttpActionResult> PutUser(string userId, Delta<RestaurantUser> patch)
+        {
+
+            RestaurantUser restaurantUser = await _db.RestaurantUsers.FindAsync(userId);
             if (restaurantUser == null)
             {
                 return NotFound();
             }
 
-            return Ok(restaurantUser);
-        }
-
-        // PUT: api/RestaurantUsers/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutRestaurantUser(string id, RestaurantUser restaurantUser)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != restaurantUser.UserId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(restaurantUser).State = EntityState.Modified;
-
             try
             {
-                await db.SaveChangesAsync();
+                patch.Patch(restaurantUser);
+                _db.Entry(restaurantUser).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!RestaurantUserExists(id))
+                if (!RestaurantUserExists(userId))
                 {
                     return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
+                throw;
             }
 
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/RestaurantUsers
         [ResponseType(typeof(RestaurantUser))]
-        public async Task<IHttpActionResult> PostRestaurantUser(RestaurantUser restaurantUser)
+        [HttpPost]
+        public async Task<IHttpActionResult> PostUser(RestaurantUser restaurantUser)
         {
+            restaurantUser.UserId = Guid.NewGuid().ToString();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            db.RestaurantUsers.Add(restaurantUser);
+            _db.RestaurantUsers.Add(restaurantUser);
 
             try
             {
-                await db.SaveChangesAsync();
+                await _db.SaveChangesAsync();
             }
             catch (DbUpdateException)
             {
@@ -101,18 +109,18 @@ namespace RestaurantWaitTime.Controllers
             return CreatedAtRoute("DefaultApi", new { id = restaurantUser.UserId }, restaurantUser);
         }
 
-        // DELETE: api/RestaurantUsers/5
-        [ResponseType(typeof(RestaurantUser))]
-        public async Task<IHttpActionResult> DeleteRestaurantUser(string id)
+        [ResponseType(typeof(User))]
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteUser(string id)
         {
-            RestaurantUser restaurantUser = await db.RestaurantUsers.FindAsync(id);
+            RestaurantUser restaurantUser = await _db.RestaurantUsers.FindAsync(id);
             if (restaurantUser == null)
             {
                 return NotFound();
             }
 
-            db.RestaurantUsers.Remove(restaurantUser);
-            await db.SaveChangesAsync();
+            _db.RestaurantUsers.Remove(restaurantUser);
+            await _db.SaveChangesAsync();
 
             return Ok(restaurantUser);
         }
@@ -121,14 +129,62 @@ namespace RestaurantWaitTime.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                _db.Dispose();
             }
             base.Dispose(disposing);
         }
 
         private bool RestaurantUserExists(string id)
         {
-            return db.RestaurantUsers.Count(e => e.UserId == id) > 0;
+            return _db.RestaurantUsers.Count(e => e.UserId == id) > 0;
         }
+
+        private bool IsRegistrated(string currentIdpUserId)
+        {
+            return _db.RestaurantUsers
+                .Where(i => i.IdpId == currentIdpUserId)
+                .Select(i => i.IdpId).ToList().Any();
+        }
+
+
+        private string CheckUserType(string idpUserId)
+        {
+            IQueryable<RestaurantUser> query = _db.RestaurantUsers;
+            var result = query.Where(i => i.IdpId == idpUserId).Select(i => i.Type).FirstOrDefault();
+            return result;
+        }
+
+        /// <summary>
+        /// Get Idpuser from Credentials
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> GetIdpUser()
+        {
+            string userName = null;
+            ClaimsPrincipal principal = User as ClaimsPrincipal;
+            string provider = principal?.FindFirst("http://schemas.microsoft.com/identity/claims/identityprovider").Value;
+
+            ProviderCredentials creds = null;
+            if (string.Equals(provider, "facebook", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<FacebookCredentials>(Request);
+            }
+            else if (string.Equals(provider, "google", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<GoogleCredentials>(Request);
+            }
+            else if (string.Equals(provider, "twitter", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<TwitterCredentials>(Request);
+            }
+
+            if (creds != null)
+            {
+                userName = $"{creds.Provider}:{creds.UserId.ToUpper()}";
+            }
+
+            return userName;
+        }
+
     }
 }
--- RestaurantWaitTime/Controllers/UsersController.cs
+++ RestaurantWaitTime/Controllers/UsersController.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Http.OData;
 using RestaurantWaitTime.Models;
+using Microsoft.Azure.Mobile.Server.Authentication;
 
 namespace RestaurantWaitTime.Controllers
 {
@@ -18,9 +19,18 @@ namespace RestaurantWaitTime.Controllers
         private RestaurantWaitTimeContext _db = new RestaurantWaitTimeContext();
 
         [HttpGet]
-        public IQueryable<User> GetAllUsers()
+        [Route("api/GetAllUsers")]
+        public IQueryable GetAllUsers()
         {
-            return _db.Users;
+            return _db.Users
+                .Select(r => new
+                {
+                    r.UserId,
+                    r.IdpId,
+                    r.Name,
+                    r.Type,
+                    r.Email
+                });
         }
 
         [Route("api/GetUser/{userId}")]
@@ -38,46 +48,42 @@ namespace RestaurantWaitTime.Controllers
             return Ok(user);
         }
 
-        // PUT: api/Users/5
+
         [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutUser(string id, User user)
+        [HttpPatch]
+        public async Task<IHttpActionResult> PutUser(string userId, Delta<User> patch)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
 
-            if (id != user.UserId)
+            User user = await _db.Users.FindAsync(userId);
+            if (user == null)
             {
-                return BadRequest();
+                return NotFound();
             }
-
-            _db.Entry(user).State = EntityState.Modified;
 
             try
             {
+                patch.Patch(user);
+                _db.Entry(user).State = EntityState.Modified;
                 await _db.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!UserExists(id))
+                if (!UserExists(userId))
                 {
                     return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
+                throw;
             }
 
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/Users
         [ResponseType(typeof(User))]
-
+        [HttpPost]
         public async Task<IHttpActionResult> PostUser(User user)
         {
+            user.UserId = Guid.NewGuid().ToString();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -104,8 +110,9 @@ namespace RestaurantWaitTime.Controllers
             return CreatedAtRoute("DefaultApi", new { id = user.UserId }, user);
         }
 
-        // DELETE: api/Users/5
+
         [ResponseType(typeof(User))]
+        [HttpDelete]
         public async Task<IHttpActionResult> DeleteUser(string id)
         {
             User user = await _db.Users.FindAsync(id);
@@ -132,6 +139,53 @@ namespace RestaurantWaitTime.Controllers
         private bool UserExists(string id)
         {
             return _db.Users.Count(e => e.UserId == id) > 0;
+        }
+
+        private bool IsRegistrated(string currentIdpUserId)
+        {
+            return _db.Users
+                .Where(i => i.IdpId == currentIdpUserId)
+                .Select(i => i.IdpId).ToList().Any();
+        }
+
+
+        private string CheckUserType(string idpUserId)
+        {
+            IQueryable<User> query = _db.Users;
+            var result = query.Where(i => i.IdpId == idpUserId).Select(i => i.Type).FirstOrDefault();
+            return result;
+        }
+
+        /// <summary>
+        /// Get Idpuser from Credentials
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> GetIdpUser()
+        {
+            string userName = null;
+            ClaimsPrincipal principal = User as ClaimsPrincipal;
+            string provider = principal?.FindFirst("http://schemas.microsoft.com/identity/claims/identityprovider").Value;
+
+            ProviderCredentials creds = null;
+            if (string.Equals(provider, "facebook", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<FacebookCredentials>(Request);
+            }
+            else if (string.Equals(provider, "google", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<GoogleCredentials>(Request);
+            }
+            else if (string.Equals(provider, "twitter", StringComparison.OrdinalIgnoreCase))
+            {
+                creds = await User.GetAppServiceIdentityAsync<TwitterCredentials>(Request);
+            }
+
+            if (creds != null)
+            {
+                userName = $"{creds.Provider}:{creds.UserId.ToUpper()}";
+            }
+
+            return userName;
         }
     }
 }
